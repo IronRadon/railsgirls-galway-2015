cec02118c60aa94bbb6a731a20055c9ea05b89d0
Fern Wood <fern@hackerone.com>
2015-05-31 08:46:32 -0700

    Add places resource with scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150531152211) do
+ActiveRecord::Schema.define(version: 20150531154112) do
+
+  create_table "places", force: :cascade do |t|
+    t.string   "name"
+    t.string   "address"
+    t.decimal  "latitude"
+    t.decimal  "longitude"
+    t.text     "description"
+    t.string   "picture"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
